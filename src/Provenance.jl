0bef6a74d0a3a59008e99cbb58f948795f68e774
Tamas Gal <tgal@km3net.de>
2021-01-07 22:14:10 +0100

    Cleanup

--- src/Provenance.jl
+++ src/Provenance.jl
@@ -6,7 +6,7 @@ using Pkg
 
 using DocStringExtensions
 
-@enum ActivityStatus unfinished finished error
+@enum ActivityStatus unfinished finished
 
 struct SystemProvenance
 end
@@ -17,16 +17,55 @@ end
 
 mutable struct Activity
     uuid::UUID
-    name::AbstractString
-    parent_activity::UUID
+    name::String
+    parent_activity::Union{UUID, Nothing}
     child_activities::Vector{UUID}
-    start::DateTime
-    stop::DateTime
+    start::Union{DateTime, Nothing}
+    stop::Union{DateTime, Nothing}
     system::SystemProvenance
-    input::Vector{AbstractString}
-    output::Vector{AbstractString}
+    input::Vector{String}
+    output::Vector{String}
     samples::Vector{SystemState}
     status::ActivityStatus
+
+    Activity(name; parent_activity=nothing) = new(
+        uuid4(),
+        name,
+        parent_activity,
+        [],
+        now(),
+        nothing,
+        SystemProvenance(),
+        [],
+        [],
+        [],
+        unfinished
+    )
+end
+
+const activities = Activity[]
+push!(activities, Activity("Main activity"))
+
+const backlog = Activity[]
+
+function startactivity(name)
+    parent_activity = activities[end]
+    activity = Activity(name, parent_activity=parent_activity.uuid)
+    push!(activities, activity)
+    push!(parent_activity.child_activities, activity.uuid)
+    nothing
+end
+
+function endactivity()
+    activity = pop!(activities)
+    finish(activity)
+    push!(backlog, activity)
+    nothing
+end
+
+function finish(activity::Activity)
+    activity.status = finished
+    nothing
 end
 
 end # module
